293e2eb05237853011cc4979479b30482bf49283
PuurfectDev-MD <new.manishd@gmail.com>
2025-09-19 18:54:15 +0400

    Ai conversation improvements and edited README

--- Essentials/Assistant/test.py
+++ Essentials/Assistant/test.py
@@ -1,25 +1,25 @@
-import pyttsx3
+import pyttsx3  # library for text to speech
 import time
 import datetime
-import speech_recognition as sr
-
+import speech_recognition as sr # for speech recognition
 import paho.mqtt.client as mqtt
 
 # MQTT Settings
-BROKER_IP = "192.168.0.116"   # your PC broker IP
-TOPIC = "jarvis/responses"
+BROKER_IP = "192.168.0.116"   # broker IP
+JARVIS_TOPIC = "jarvis/responses"
 CONTROL_TOPIC = "jarvis/control"
 AI_RESPONSE = "ai/responses"
+AI_REQUEST = "ai/requests"
+
 listening = False
-
 ai_speaking = False
+ai_mode= False
 
-
-r = sr.Recognizer()
-keywords = [("jarvis", 1), ("hey jarvis", 1), ("wake up", 1), ("Maya",1)]
+r = sr.Recognizer()  #keeps the laptops mic as the source for the audio
+keywords = [("jarvis", 1), ("hey jarvis", 1), ("wake up", 1), ("Maya",1)]  # wake up words
 
 rate =100  # voice speed
-engine = pyttsx3.init()
+engine = pyttsx3.init()  
 voices = engine.getProperty('voices')  #get the different voices
 engine.setProperty('voice', voices[0].id) #selection and setting of the voice
 engine.setProperty('rate', rate+50) #setting the rate
@@ -27,13 +27,13 @@ engine.setProperty('rate', rate+50) #setting the rate
 
 
 
-def on_message(client, userdata, msg):
+def on_message(client, userdata, msg):  # when the broker recieves a message
     global listening, ai_speaking
     topic = msg.topic
     payload = msg.payload.decode()
     print(f"📩 Message received on {topic}: {payload}")
 
-    if topic == CONTROL_TOPIC:
+    if topic == CONTROL_TOPIC:  #identifies which topic/stream the data is received from
         if payload == "wake":
             print("⚡ Wake command received from ESP32")
             listening = True
@@ -46,20 +46,22 @@ def on_message(client, userdata, msg):
         ai_speaking = False
 
 
-def send_to_mqtt(message):
-    client.publish(TOPIC, message)
+def send_to_mqtt(topic, message):  #to send data to a stream in the broker
+    client.publish(topic, message)
     print(f"📤 Sent to MQTT: {message}")
 
 
 
-def Speak(text):
+def Speak(text):  # uses text to speech lib
     print(f"Jarvis: {text}")
     engine.say(text)
     time.sleep(2)
     engine.runAndWait()
   
 
-def recognize_main():
+def recognize_main():  #to recognize commands using the audio source
+    global listening
+    global ai_mode
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         print("Listening for commands...")
@@ -82,12 +84,12 @@ def recognize_main():
                 Speak("Good evening, sir")
         elif "led" in data and "on" in data:
             print("You: LED On")
-            send_to_mqtt("LED on")
+            send_to_mqtt(CONTROL_TOPIC,"LED on")
             Speak("The LED is on sir.")
             
         elif "led" in data and "off" in data:
             print("You: LED off")
-            send_to_mqtt("LED off")
+            send_to_mqtt(CONTROL_TOPIC,"LED off")
             Speak("The LED is off sir.")
         elif "chantal" in data and "think" in data:
             print("You want to know about her?")
@@ -96,9 +98,10 @@ def recognize_main():
         elif "connect" in data and "ai" in data:
             print("Connecting with AI")
             Speak("Connecting with AI sir. Give me a moment.")
-            send_to_mqtt("Connect AI")
-            global listening
+            send_to_mqtt(CONTROL_TOPIC,"Connect AI")
+           
             listening = False
+            ai_mode = True
 
         elif "end conversation" in data:
             Speak("Ending our chat. Have a nice day, sir")
@@ -116,33 +119,53 @@ def recognize_main():
 
 
 
-def wakeupCall():
+def wakeupCall():   # to wake the assistant up
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         print("Say Jarvis...")
-        audio = r.listen(source)
+        audio = r.listen(source)  #listens to the mic
     try:
-        speech_as_text= r.recognize_sphinx(audio, keyword_entries=keywords)
+        speech_as_text= r.recognize_sphinx(audio, keyword_entries=keywords)  # matches the decoded speech to the keywords
         print(f" You: {speech_as_text}")
         if "jarvis" in speech_as_text or "hey jarvis" in speech_as_text or "wake up" in speech_as_text:
           Speak("Yes sir?")
-          recognize_main()  #main repeating loop after call
-
+          recognize_main()  #main command detection function
     except sr.UnknownValueError:
            pass
     
 
 
 # ---------- MQTT SETUP ----------
-client = mqtt.Client(client_id="jarvis_pc")
-client.on_message = on_message
+client = mqtt.Client(client_id="jarvis_pc")   
+client.on_message = on_message  #after a message is recieved - this function is automatically called
 client.connect(BROKER_IP, 1883, 60)
+client.subscribe(JARVIS_TOPIC)
 client.subscribe(CONTROL_TOPIC)
 client.subscribe(AI_RESPONSE) 
-client.loop_start()
+client.loop_start()  
 
 
 while True:
-    if listening and not ai_speaking:
+    if listening and not ai_speaking and not ai_mode: #only listens to the wakeup call if not speaking
         wakeupCall()
+    if ai_mode and not ai_speaking:  # for ai conversations until ended 
+        while ai_mode and not ai_speaking:
+            print("Listening for ai query")
+            with sr.Microphone() as source:
+                r.adjust_for_ambient_noise(source)
+                audio = r.listen(source, phrase_time_limit=6)
+            try:
+                data = r.recognize_google(audio).lower()
+                print(f" You: {data}")
+                send_to_mqtt(AI_REQUEST, data)
+                ai_speaking = True
+
+                if "go back" in data or "end chat" in data or "close chat" in data:
+                    print("Closing connection with AI")
+                    Speak("Closing connection with AI")
+                    ai_mode = False
+                
+            except sr.UnknownValueError:
+                pass
+
     time.sleep(0.2)
--- README.md
+++ README.md
@@ -2,12 +2,34 @@
 
 This project combines **ESP32 hardware** (running MicroPython) with a **Python server** on my laptop.
 
+
+#Functionalities:
+ > Schedule Tracker
+ > Prodcutivity Statistics
+ > Calnder api integrations to streamline daily routines
+ > Alarms
+ > Personal Assistant
+ > AI conversation - (GPT 3.5 mini currently in use)
+
 ## Structure
-- `esp32/` → ESP32 code for the assistant
-- `server/` → PC-side server code
+- `Esp32 Flash` → These are the content of the flash - excluding the main package(code)
+- `Esp32- Thonny` → 
+         Library : The necessary libraries
+         Package : Upload the entire folder to flash of esp32 to run the system
+         Snippets: Individual functionalities broken up 
+         Test: For testing and improvement purposes
+
+- `Essentials` → 
+         Assistant : For speech communcation - to access the mic of the PC
+         Audio Type conversion : Server to covert mp3 into .wav for esp32 to play it from the board.
 - `README.md` → this file (documentation)
 
-## How to Run
-1. Flash ESP32 with MicroPython.
-2. Upload code from `esp32/` folder.
-3. Run `python server/main.py` to start the server.
+
+
+## To begin virtual environment:
+In cmd - cd '{path of the main folder}'
+         ./{name of the virtual env folder}/Scripts/Activate.ps1
+
+## To being the MQTT broker:
+cmd : mosquitto -c "C:\\Program Files\\mosquitto\\mosquitto.conf" -v
+*** -v means verbose, so you’ll see connection logs.
